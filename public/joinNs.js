6cbac1fb1534b61e9e836060c500db004cc3009f
anfer <anfer@sube.la>
2021-06-07 20:09:11 -0600

    socketio points to server

--- public/joinNs.js
+++ public/joinNs.js
@@ -1,72 +1,71 @@
-function joinNs(endpoint){
-if(nsSocket){
+function joinNs(endpoint) {
+  if (nsSocket) {
     nsSocket.close();
 
     //remove eventLIstener befor its added again
-    document.querySelector('#user-input').removeEventListener('submit',formSubmission);
-}
+    document
+      .querySelector("#user-input")
+      .removeEventListener("submit", formSubmission);
+  }
+  nsSocket = io(`http://www.slackclone.anferneecastillo.com/${endpoint}`, {
+    query: {
+      username,
+    },
+  });
 
- nsSocket = io(`http://10.0.0.103:3000/${endpoint}`,{ query:{
-    username
-}});
-
-nsSocket.on('nsRoomLoad',function(nsRooms){
-
+  nsSocket.on("nsRoomLoad", function (nsRooms) {
     let roomList = document.querySelector(".room-list");
-    roomList.innerHTML="";
-    nsRooms.forEach((room)=>{
-        let glyph;
-        if(room.privateRoom){
-            glyph='lock'
-        }
-        else{
-            glyph= 'globe'  
-        }
-        roomList.innerHTML +=`<li class="room" > <span class="glyphicon glyphicon-${glyph}"></span>${room.roomtitle}</li>`
+    roomList.innerHTML = "";
+    nsRooms.forEach((room) => {
+      let glyph;
+      if (room.privateRoom) {
+        glyph = "lock";
+      } else {
+        glyph = "globe";
+      }
+      roomList.innerHTML += `<li class="room" > <span class="glyphicon glyphicon-${glyph}"></span>${room.roomtitle}</li>`;
     });
-    let roomNodes= document.getElementsByClassName('room');
-    Array.from(roomNodes).forEach((room)=>{
-        room.addEventListener('click',(e)=>{
-            joinRoom(e.target.innerText);
-        });
-    })
-    const topRoom = document.querySelector('.room');
-    const topRoomName= topRoom.innerText;
+    let roomNodes = document.getElementsByClassName("room");
+    Array.from(roomNodes).forEach((room) => {
+      room.addEventListener("click", (e) => {
+        joinRoom(e.target.innerText);
+      });
+    });
+    const topRoom = document.querySelector(".room");
+    const topRoomName = topRoom.innerText;
     joinRoom(topRoomName);
-});
+  });
 
-
-nsSocket.on('messageToClients',function(msg){
-
+  nsSocket.on("messageToClients", function (msg) {
     const newMsg = buildHTML(msg);
-    document.querySelector('#messages').innerHTML +=newMsg;
-   
-});
+    document.querySelector("#messages").innerHTML += newMsg;
+  });
 
-
-document.querySelector('.message-form').addEventListener('submit',formSubmission);
+  document
+    .querySelector(".message-form")
+    .addEventListener("submit", formSubmission);
 }
 
-function formSubmission(event){
-    event.preventDefault();
-    var input = document.querySelector('#user-message-input');
-    const newMessage= input.value;
-    nsSocket.emit('newMessageToServer',{text:newMessage});
-    input.value='';
+function formSubmission(event) {
+  event.preventDefault();
+  var input = document.querySelector("#user-message-input");
+  const newMessage = input.value;
+  nsSocket.emit("newMessageToServer", { text: newMessage });
+  input.value = "";
 
-    const messagesUl = document.querySelector('#messages');
+  const messagesUl = document.querySelector("#messages");
 
-    // history.forEach(function(msg){
-    //     const newMessage = buildHTML(msg);
-    //     const currentMessage = messagesUl.innerHTML;
-    //     messagesUl.innerHTML = currentMessage+newMessage;
-    // });
-    messagesUl.scrollTo(0,messagesUl.scrollHeight);
-};
+  // history.forEach(function(msg){
+  //     const newMessage = buildHTML(msg);
+  //     const currentMessage = messagesUl.innerHTML;
+  //     messagesUl.innerHTML = currentMessage+newMessage;
+  // });
+  messagesUl.scrollTo(0, messagesUl.scrollHeight);
+}
 
-function buildHTML(msg){
-    const convertedDate = new Date(msg.time).toLocaleDateString();
-   const newHTML= `
+function buildHTML(msg) {
+  const convertedDate = new Date(msg.time).toLocaleDateString();
+  const newHTML = `
    <li class="message-wrapper">
    <div class="user-image" >
    <img style="width:30px; height:30px" src="https://www.automotiveone.com/wp-content/uploads/2019/02/placeholder-user-image.jpg" />
@@ -74,6 +73,6 @@ function buildHTML(msg){
 <div class="user-message">
    <div class="user-name-time">${msg.username} <span>${convertedDate}</span></div>
    <div class="message-text">${msg.text}</div>
-</div>  </li>` 
-return newHTML;
+</div>  </li>`;
+  return newHTML;
 }
--- public/scripts.js
+++ public/scripts.js
@@ -1,26 +1,23 @@
 const username = prompt("Type in your username");
-const socket = io("http://10.0.0.103:3000",{
-    query:{
-        username
-    }
+const socket = io("http://www.slackclone.anferneecastillo.com", {
+  query: {
+    username,
+  },
 });
 
+let nsSocket = "";
+socket.on("nslist", function (nsData) {
+  let namespacesDiv = document.querySelector(".namespaces");
+  namespacesDiv.innerHTML = "";
+  nsData.forEach((ns) => {
+    namespacesDiv.innerHTML += `<div class="namespace" ns=${ns.endpoint}><img src="${ns.img}" /></div>`;
+  });
 
-
-let nsSocket="";
-socket.on('nslist',function(nsData){
-    let namespacesDiv = document.querySelector('.namespaces');
-    namespacesDiv.innerHTML ="";
-    nsData.forEach((ns) => {
-        namespacesDiv.innerHTML +=`<div class="namespace" ns=${ns.endpoint}><img src="${ns.img}" /></div>`
+  Array.from(document.getElementsByClassName("namespace")).forEach((elem) => {
+    elem.addEventListener("click", function (e) {
+      const nsEndpoint = elem.getAttribute("ns");
+      joinNs(nsEndpoint);
     });
-
-   Array.from(document.getElementsByClassName('namespace')).forEach((elem)=>{
-        elem.addEventListener('click',function(e){
-            const nsEndpoint = elem.getAttribute('ns');
-            joinNs(nsEndpoint);
-        });
-    });
-   joinNs('/wiki');
+  });
+  joinNs("/wiki");
 });
-
